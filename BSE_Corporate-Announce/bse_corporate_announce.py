6ec210564dd4f4e7d8e98911613f084c4bf54a14
Pranjal Banka <61748950+pranjalbanka@users.noreply.github.com>
2020-11-11 07:46:10 +0530

    Add files via upload

---
+++ BSE_Corporate-Announce/bse_corporate_announce.py
@@ -0,0 +1,55 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import pync
+import os
+import datetime
+
+driver = webdriver.Chrome()
+driver.get("https://www.bseindia.com/corporates/ann.html")
+driver.find_element_by_xpath("//select[@id='ddlAnnType']/option[text()='Equity']").click()
+driver.find_element_by_xpath("//select[@id='ddlPeriod']/option[text()='Result']").click()
+
+
+stockEvent = open("stockEvent.txt","r") 
+stocks = stockEvent.readlines()
+resultsOut=[]
+
+while True:
+
+	for stock in stocks:
+		securityName=stock
+
+		#securityName = input("Enter the stock name: ")
+
+		driver.find_element_by_xpath("//input[@id='scripsearchtxtbx']").clear()
+
+		time.sleep(1)
+
+		securityNameInput = driver.find_element_by_xpath("//input[@id='scripsearchtxtbx']")
+		securityNameInput.send_keys(securityName)
+		securityNameInput.submit()
+
+		time.sleep(5)
+
+		securityNameClick = driver.find_element_by_xpath("//ul[@id='ulSearchQuote2']").click()
+
+		submit = driver.find_element_by_xpath("//input[@id='btnSubmit']").click()
+		time.sleep(5)
+		pdf = driver.find_element_by_xpath("//td[@class='tdcolumngrey ng-scope']/a[@href]")
+
+		if pdf != None:
+			pync.notify(securityName +'\n'+ str(datetime.datetime.now().time()), title='Results!!!',sound="default",open=driver.find_element_by_xpath("//td[@class='tdcolumngrey ng-scope']/a").get_attribute('href'),appIcon="bse-logo.png")
+			resultsOut.append(securityName)
+
+		else:
+			time.sleep(5)
+			#driver.find_element_by_xpath("//td[@class='tdcolumngrey ng-scope']/a[@href]").click()
+
+	for results in resultsOut:
+		stocks.remove(results)
+
+file1.close()
+
+
+
